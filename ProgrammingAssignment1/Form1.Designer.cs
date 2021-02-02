32636acea2de2d68ea031f68026a34aabf44c670
Nathan2203 <jonathanbenedict01@gmail.com>
2021-02-02 08:34:51 +0700

    'onprogress'

---
+++ ProgrammingAssignment1/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace ProgrammingAssignment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawingScreen = new System.Windows.Forms.PictureBox();
+            this.toolPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawLineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dottedLineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingScreen)).BeginInit();
+            this.toolPanel.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // drawingScreen
+            // 
+            this.drawingScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drawingScreen.Location = new System.Drawing.Point(0, 28);
+            this.drawingScreen.Name = "drawingScreen";
+            this.drawingScreen.Size = new System.Drawing.Size(819, 458);
+            this.drawingScreen.TabIndex = 0;
+            this.drawingScreen.TabStop = false;
+            this.drawingScreen.Paint += new System.Windows.Forms.PaintEventHandler(this.drawingScreen_Paint);
+            this.drawingScreen.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawingScreen_MouseDown);
+            this.drawingScreen.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawingScreen_MouseMove);
+            // 
+            // toolPanel
+            // 
+            this.toolPanel.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.toolPanel.Controls.Add(this.numericUpDown1);
+            this.toolPanel.Controls.Add(this.label1);
+            this.toolPanel.Controls.Add(this.groupBox1);
+            this.toolPanel.Controls.Add(this.button1);
+            this.toolPanel.Controls.Add(this.btnClear);
+            this.toolPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.toolPanel.Location = new System.Drawing.Point(582, 28);
+            this.toolPanel.Name = "toolPanel";
+            this.toolPanel.Size = new System.Drawing.Size(237, 458);
+            this.toolPanel.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(127, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(13, 183);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(98, 48);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "&Clear ";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(222, 66);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Colors:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 35);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pictureBox2.Location = new System.Drawing.Point(64, 25);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 35);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox3.Location = new System.Drawing.Point(104, 25);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 35);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox4.Location = new System.Drawing.Point(144, 25);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(34, 35);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Width:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(78, 112);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 26);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.drawLineToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(819, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // drawLineToolStripMenuItem
+            // 
+            this.drawLineToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dottedLineToolStripMenuItem});
+            this.drawLineToolStripMenuItem.Name = "drawLineToolStripMenuItem";
+            this.drawLineToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.drawLineToolStripMenuItem.Text = "Draw Line";
+            // 
+            // dottedLineToolStripMenuItem
+            // 
+            this.dottedLineToolStripMenuItem.Name = "dottedLineToolStripMenuItem";
+            this.dottedLineToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.dottedLineToolStripMenuItem.Text = "Dotted Line";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 486);
+            this.Controls.Add(this.toolPanel);
+            this.Controls.Add(this.drawingScreen);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Draw Line";
+            ((System.ComponentModel.ISupportInitialize)(this.drawingScreen)).EndInit();
+            this.toolPanel.ResumeLayout(false);
+            this.toolPanel.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawingScreen;
+        private System.Windows.Forms.Panel toolPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drawLineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dottedLineToolStripMenuItem;
+    }
+}
+
---
+++ ProgrammingAssignment1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgrammingAssignment1
+{
+    public partial class Form1 : Form
+    {
+        //int x1=120, y1=50, x2=300, y2=200, temp;
+        //int x1 = 120, y1 = 100, x2 = 120, y2 = 300, temp;
+        Point start_point, end_point;
+        Color colorLine = Color.Black;
+        Bitmap myBitmap;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawingScreen.Image = myBitmap;
+            //Line();
+        }
+
+        private void drawingScreen_MouseDown(object sender, MouseEventArgs e)
+        {
+            //get new start_point
+            if (e.Button == MouseButtons.Left)
+            {
+                start_point = new Point(e.X, e.Y);
+                //MessageBox.Show(" " + e.X + " "+ e.Y);
+            }
+        }
+
+        private void drawingScreen_Paint(object sender, PaintEventArgs e)
+        {
+            //int x1 = 120, y1 = 100, x2 = 120, y2 = 300, temp
+            int x1 = start_point.X, y1 = start_point.Y, x2 = end_point.X, y2 = end_point.Y;
+            int temp;
+            drawingScreen.Image = myBitmap;
+            int dx, dy, d, x, y;
+            float m;
+            if ((x2 - x1) == 0) // If it's Case 1 & 2
+                m = (y2 - y1);
+            else
+                m = (y2 - y1) / (x2 - x1);
+
+            if (Math.Abs(m) < 1)
+            {
+                if (x1 > x2)
+                {
+                    temp = x1;
+                    x1 = x2;
+                    x2 = temp;
+
+                    temp = y1;
+                    y1 = y2;
+                    y2 = temp;
+                }
+
+                dx = Math.Abs(x2 - x1);
+                dy = Math.Abs(y2 - y1);
+
+                d = 2 * dy - dx; // Initial parametric value
+
+                x = x1;
+                y = y1;
+
+                while (x <= x2)
+                {
+                    //set pixel
+                    myBitmap.SetPixel(x, y, colorLine);
+                    x++;
+
+                    if (d >= 1)
+                    {
+                        if (m < 1)
+                            y += 1;
+                        else
+                            y -= 1;
+                        d = d + 2 * dy - 2 * dx;
+                    }
+                    else
+                    {
+                        d = d + 2 * dy;
+                    }
+                }
+            }
+
+            if (Math.Abs(m) >= 1)
+            {
+                if (y1 > y2)
+                {
+                    temp = x1;
+                    x1 = x2;
+                    x2 = temp;
+
+                    temp = y1;
+                    y1 = y2;
+                    y2 = temp;
+                }
+
+                dx = Math.Abs(x2 - x1);
+                dy = Math.Abs(y2 - y1);
+
+                d = 2 * dx - dy; // Initial parametric value
+
+                x = x1;
+                y = y1;
+
+                while (y <= y2)
+                {
+                    //set pixel
+                    myBitmap.SetPixel(x, y, colorLine);
+                    y++;
+
+                    if (d >= 1)
+                    {
+                        if (m >= 1)
+                            x += 1;
+                        else
+                            x -= 1;
+                        d = d + 2 * dx - 2 * dy;
+                    }
+                    else
+                    {
+                        d = d + 2 * dx;
+                    }
+                }
+            }
+        }
+
+        private void drawingScreen_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                end_point = new Point(e.X, e.Y);
+                drawingScreen.Invalidate();
+            }
+        }
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            myBitmap = new Bitmap(drawingScreen.Width, drawingScreen.Height);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            drawingScreen.Image = null;
+            drawingScreen.BackColor = Color.White;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            colorLine = pictureBox1.BackColor;
+        }
+
+        public void Line(Point p1, Point p2)
+        {
+            
+            
+
+
+
+        }
+    }
+}
